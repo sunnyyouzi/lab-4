10d418c3e9f5ceb45edb19722cd3d9995694e467
sunnyyouzi <fengmingyang@fengmingyangdemacbook-air.local>
2016-06-10 20:22:50 +0800

    Initial Commit

---
+++ lab－4/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  lab－4
+//
+//  Created by 冯铭扬 on 16/6/10.
+//  Copyright © 2016年 冯铭扬. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
